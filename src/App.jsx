2058a7b0bfcd9686bf43611d1c718186be906fdb
Luciana B Ferrand <lbferrand@gmail.com>
2025-02-14 18:58:51 -0500

    Set the react app for movie app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked, title]);
+
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      {/* This is how to set a button */}
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "♡"}
+      </button>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 const App = () => {
-  return <h2>Functional Arrow Component</h2>;
+  return (
+    <div className="card-container">
+      <Card
+        title="The Notebook"
+        ratings={5}
+        actors={[{ name: "Ryan Gosling" }, { name: "Rachel McAdams" }]}
+      />
+      <Card
+        title="Sense & Sensibility"
+        ratings={5}
+        actors={[{ name: "Emma Thompson" }, { name: "Kate Winslet" }]}
+      />
+      <Card title="Pride & Predige" />
+      <Card title="Under the Tuscany Sun" />
+      <Card title="The Bridge of Madison County" />
+    </div>
+  );
 };
 
 export default App;
